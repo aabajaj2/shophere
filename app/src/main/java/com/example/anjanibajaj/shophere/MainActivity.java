0ff7d92e2e95c5d9d4849c332bddfe68e72b769f
Anjani Bajaj <aabajaj2@dons.usfca.edu>
2017-07-05 18:08:05 +0530

    update documentation and optimized imports

--- app/src/main/java/com/example/anjanibajaj/shophere/MainActivity.java
+++ app/src/main/java/com/example/anjanibajaj/shophere/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.anjanibajaj.shophere;
 
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,19 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.anjanibajaj.shophere.Utils.Constants;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
